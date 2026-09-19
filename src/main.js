8fa134b4204bbc3b49f10b4f7e7dfe7e36239662
crigley <48026372+wik60@users.noreply.github.com>
2026-09-19 20:15:48 +0200

    Add front-to-back product hover

--- src/main.js
+++ src/main.js
@@ -269,6 +269,8 @@ sizeButtons.forEach((button) => {
 viewButtons.forEach((button) => button.addEventListener("click", () => setView(button.dataset.view)));
 els.prevImage.addEventListener("click", () => setView(activeView === "front" ? "back" : "front"));
 els.nextImage.addEventListener("click", () => setView(activeView === "front" ? "back" : "front"));
+els.imageStage.addEventListener("mouseenter", () => setView("back"));
+els.imageStage.addEventListener("mouseleave", () => setView("front"));
 els.imageStage.addEventListener("dblclick", () => setView(activeView === "front" ? "back" : "front"));
 
 els.sizeGuideToggle.addEventListener("click", () => {
